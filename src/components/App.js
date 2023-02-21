4d9a95dd1e01550adcf6c16c412a3770a09a3cc1
Sameer Khan <115018276+Sameeerkhan78@users.noreply.github.com>
2023-02-21 10:50:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,22 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
-import Login from './Login';
-import Home from './Home';
+import React, { useState } from "react";
+import "./styles/App.css";
+import Login from "./Login";
+import Home from "./Home";
 
 const App = () => {
   const [logedUser, setLogedUser] = useState(false);
 
   const onLogin = (username, password) => {
-    if (username === 'admin' && password === 'admin@123')
-      setLogedUser(true);
-    else
-      alert('Please Enter Correct UserNmae PassWord!')
-  }
+    if (username === "sameer" && password === "sameer@khan") setLogedUser(true);
+    else alert("Please Enter Correct UserNmae PassWord!");
+  };
   return (
     <div id="main">
       {!logedUser && <Login onLogin={onLogin} />}
       {logedUser && <Home />}
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
